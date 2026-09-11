0220ce5edccb5d23adeef5a04e49a0d4ac4ee706
lakshitha <lakshithaneelakandan@gmail.com>
2026-09-11 18:47:20 +0530

    root folder create

---
+++ SIH-hackathon-main/root/script.js
@@ -0,0 +1,35 @@
+// Get Started Button
+
+function showMessage() {
+
+    alert("Welcome to SkillBridge!");
+
+}
+
+
+// Skill Gap Button
+
+function checkSkills() {
+
+    alert(
+        "Your Skill Analysis:\n\n" +
+        "HTML - Good\n" +
+        "CSS - Good\n" +
+        "JavaScript - Need Improvement\n" +
+        "Python - Good"
+    );
+
+}
+
+
+// Apply Button
+
+function apply(course) {
+
+    alert(
+        "You selected:\n\n" +
+        course +
+        "\n\nApplication submitted successfully!"
+    );
+
+}
